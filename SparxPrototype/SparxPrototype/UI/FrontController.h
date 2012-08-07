243af55f81b717f3d7bf1911fd9181dd0955540c
Alex Lapinski <alexander.lapinski@razorfishhealth.com>
2012-08-07 13:37:10 -0400

    added qr scanning

--- SparxPrototype/SparxPrototype/UI/FrontController.h
+++ SparxPrototype/SparxPrototype/UI/FrontController.h
@@ -8,13 +8,41 @@
 
 #import <UIKit/UIKit.h>
 #import <AddressBookUI/AddressBookUI.h>
+#import "DecoderDelegate.h"
+@class QRImagePickerController;
+@class OverlayView;
 
-@interface FrontController : UIViewController <ABPeoplePickerNavigationControllerDelegate>
+static const int kGenerateViewTag = 1;
+static const int kScanViewTag = 2;
+
+@interface FrontController : UIViewController <ABPeoplePickerNavigationControllerDelegate, 
+    UITabBarDelegate, 
+    UIImagePickerControllerDelegate,
+    UINavigationControllerDelegate,
+    DecoderDelegate>
 {
+    IBOutlet UITabBar* navBar;
+    IBOutlet UITabBarItem* generateNavButton;
+    IBOutlet UITabBarItem* scanNavButton;
+    IBOutlet UIView* viewport;
+   
+    IBOutlet UIView* generateView;
     IBOutlet UIImageView* qrView;
     IBOutlet UIButton* selectContactButton;
+    
+    IBOutlet UIView* scanView;
+    IBOutlet UILabel* scanOutput;
+    IBOutlet UIButton* scanCodeButton;
+    
+    @private
+    UIImagePickerController*  _imagePicker;
+    OverlayView*              _overlayView;
+    NSTimer*                  _timer;
+    QRDecoder*                _decoder;
+
 }
 
 -(IBAction)handleSelectContactButtonPressed:(id)sender;
+-(IBAction)handleScanCodeButtonPressed:(id)sender;
 
 @end
--- SparxPrototype/SparxPrototype/UI/FrontController.m
+++ SparxPrototype/SparxPrototype/UI/FrontController.m
@@ -9,8 +9,14 @@
 #import <QuartzCore/QuartzCore.h>
 #import <QREncoder/QREncoder.h>
 #import <AddressBookUI/AddressBookUI.h>
+#import "QRImagePickerController.h"
+#import "OverlayView.h"
+#import "QRDecoder.h"
+#import "TwoDDecoderResult.h"
 
 #import "FrontController.h"
+
+static const NSTimeInterval kTakePictureTimeInterval = 5;
 
 @interface FrontController(hidden)
 -(void)updateQRCode:(ABRecordRef)person;
@@ -18,11 +24,9 @@
 @implementation FrontController(hidden)
 -(void)updateQRCode:(ABRecordRef)person
 {
-    NSString* firstName = (__bridge_transfer NSString*)ABRecordCopyValue(person, kABPersonFirstNameProperty);
-    
-    NSString* lastName = (__bridge_transfer NSString*)ABRecordCopyValue(person, kABPersonLastNameProperty);
-    
-    UIImage* image = [QREncoder encode: [NSString stringWithFormat:@"%@-%@",firstName,lastName]];
+    ABRecordID recordID = ABRecordGetRecordID(person);
+        
+    UIImage* image = [QREncoder encode: [NSString stringWithFormat:@"%@",recordID]];
     
     [qrView setImage:image];
     [qrView layer].magnificationFilter = kCAFilterNearest;
@@ -39,6 +43,9 @@ static int const kPadding = 5;
 {
     [super viewDidLoad];
     
+    [navBar setSelectedItem:generateNavButton];
+    [self tabBar:navBar didSelectItem:generateNavButton];
+    
     self.view.backgroundColor = [UIColor whiteColor];
 }
 
@@ -50,6 +57,42 @@ static int const kPadding = 5;
     [picker setPeoplePickerDelegate:self];
     
     [self presentModalViewController:picker animated:YES];
+}
+
+-(IBAction)handleScanCodeButtonPressed:(id)sender
+{
+    UIImagePickerControllerSourceType type = UIImagePickerControllerSourceTypeCamera;
+    
+    if( ![UIImagePickerController isSourceTypeAvailable:type] ) {
+        UIAlertView* alertView = [[UIAlertView alloc]
+                                  initWithTitle:@"Not a supported device"
+                                  message:@"You need a camera to run this app"
+                                  delegate:self
+                                  cancelButtonTitle:@"Darn"
+                                  otherButtonTitles:nil];
+        
+        [alertView show];
+        
+    } else {
+        _imagePicker = [[QRImagePickerController alloc] init];
+        
+        _overlayView = [[OverlayView alloc] initWithFrame:[UIScreen mainScreen].bounds];
+        
+        _imagePicker.delegate = self;
+        _imagePicker.allowsEditing = NO;
+        _imagePicker.showsCameraControls = NO;
+        _imagePicker.cameraOverlayView = _overlayView;
+        
+        [self presentModalViewController:_imagePicker animated:YES];
+        
+        _timer = [NSTimer
+                  scheduledTimerWithTimeInterval: kTakePictureTimeInterval
+                  target: self
+                  selector: @selector(takePicture:)
+                  userInfo: nil
+                  repeats: YES];
+    }
+
 }
 
 #pragma mark -
@@ -71,4 +114,116 @@ static int const kPadding = 5;
 {
     return NO;
 }
+#pragma mark -
+
+#pragma mark UITabBarDelegate
+-(void)tabBar:(UITabBar *)tabBar didSelectItem:(UITabBarItem *)item
+{
+    if(item == generateNavButton ) {
+        if([[viewport subviews] count] > 0) {
+            for(id child in [viewport subviews])
+            {
+                [child removeFromSuperview];
+            }
+        }
+        
+        [viewport addSubview:generateView];
+    }
+    
+    else if(item == scanNavButton) {
+        if([[viewport subviews] count] > 0) {
+            for(id child in [viewport subviews])
+            {
+                [child removeFromSuperview];
+            }
+        }
+        
+        [viewport addSubview:scanView];
+    }
+}
+#pragma mark -
+
+#pragma mark UIImagePickerDelegate
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker {
+	[self dismissModalViewControllerAnimated:YES];
+}
+
+- (UIImage*) scaledImage:(UIImage*)baseImage {
+	CGSize targetSize = CGSizeMake(320, 480);	
+	CGRect scaledRect = CGRectZero;
+    
+	CGFloat scaledX = 480 * baseImage.size.width / baseImage.size.height;
+    
+	scaledRect.origin = CGPointMake(0, 0.0);
+	scaledRect.size.width  = scaledX;
+	scaledRect.size.height = 480;
+    
+	UIGraphicsBeginImageContext(targetSize);
+	[baseImage drawInRect:scaledRect];
+	UIImage* result = UIGraphicsGetImageFromCurrentImageContext();
+	UIGraphicsEndImageContext();
+    
+    return result;
+}
+
+- (UIImage*) croppedImage:(UIImage*)baseImage {
+	CGSize targetSize = _overlayView.cropRect.size;	
+    
+	UIGraphicsBeginImageContext(targetSize);
+	[baseImage drawAtPoint:CGPointMake(-_overlayView.cropRect.origin.x, -_overlayView.cropRect.origin.y)];
+	UIImage* result = UIGraphicsGetImageFromCurrentImageContext();
+	UIGraphicsEndImageContext();
+    
+    return result;
+}
+
+- (void) imagePickerController:(UIImagePickerController*)picker 
+ didFinishPickingMediaWithInfo:(NSDictionary*)info {
+    UIImage* image = [info objectForKey:UIImagePickerControllerOriginalImage];
+    
+    UIImage* scaled = [self scaledImage:image];
+    
+    if( nil == _decoder ) {
+        _decoder = [[QRDecoder alloc] init];
+        _decoder.delegate = self;
+    }
+    [_decoder decodeImage:scaled cropRect:_overlayView.cropRect];
+    
+    _overlayView.image = [self croppedImage:scaled];
+}
+#pragma mark -
+
+#pragma mark NSTimer
+- (void)takePicture:(NSTimer*)theTimer {
+    [_imagePicker takePicture];
+}
+#pragma mark -
+
+#pragma mark DecoderDelegate
+- (void)decoder:(QRDecoder *)decoder willDecodeImage:(UIImage *)image usingSubset:(UIImage *)subset {
+}
+
+- (void)decoder:(QRDecoder *)decoder decodingImage:(UIImage *)image usingSubset:(UIImage *)subset progress:(NSString *)message {
+}
+
+- (void)decoder:(QRDecoder *)decoder didDecodeImage:(UIImage *)image usingSubset:(UIImage *)subset withResult:(TwoDDecoderResult *)result {
+
+    _overlayView.points = result.points;
+
+    ABRecordID record = (ABRecordID)result.text;
+    
+    ABRecordRef person = ABAddressBookGetPersonWithRecordID(ABAddressBookCreate(), record);
+    
+    NSString* name = (__bridge_transfer NSString*)ABRecordCopyCompositeName(person);
+    
+    [scanOutput setText:name];
+    
+	[self dismissModalViewControllerAnimated:YES];
+}
+
+- (void)decoder:(QRDecoder *)decoder failedToDecodeImage:(UIImage *)image usingSubset:(UIImage *)subset reason:(NSString *)reason {
+}
+
+
+
 @end
